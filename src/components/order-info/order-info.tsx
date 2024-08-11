e9ccd3623f8460c5c11dbe0e336fe269a3664ec6
Artur <kotorinartur@gmail.com>
2024-08-11 10:12:23 +0300

    ПР11: полная реализация функционала

--- src/components/order-info/order-info.tsx
+++ src/components/order-info/order-info.tsx
@@ -1,23 +1,35 @@
-import { FC, useMemo } from 'react';
+import { FC, useEffect, useMemo, useState } from 'react';
 import { Preloader } from '../ui/preloader';
 import { OrderInfoUI } from '../ui/order-info';
-import { TIngredient } from '@utils-types';
+import { TIngredient, TOrder } from '@utils-types';
+import { useDispatch, useSelector } from '../../services/store';
+import { ingredientsSelector } from '../../services/ingredients/slice';
+import { getOrderByNum } from '../../services/order/action';
+import { useParams } from 'react-router-dom';
+import { ordersSelector } from '../../services/order/slice';
 
 export const OrderInfo: FC = () => {
   /** TODO: взять переменные orderData и ingredients из стора */
-  const orderData = {
-    createdAt: '',
-    ingredients: [],
-    _id: '',
-    status: '',
-    name: '',
-    updatedAt: 'string',
-    number: 0
-  };
+  const dispatch = useDispatch();
+  const param = useParams();
+  const number = Number(param.number);
+  const [orderData, setOrderData] = useState<TOrder | null>(null);
+  const ingredients: TIngredient[] = useSelector(ingredientsSelector);
+  const data = useSelector(ordersSelector);
+  useEffect(() => {
+    if (number) {
+      const order: TOrder | undefined = data.find(
+        (item) => item.number === number
+      );
 
-  const ingredients: TIngredient[] = [];
+      if (order) {
+        setOrderData(order);
+      } else {
+        dispatch(getOrderByNum(number));
+      }
+    }
+  }, [dispatch, number]);
 
-  /* Готовим данные для отображения */
   const orderInfo = useMemo(() => {
     if (!orderData || !ingredients.length) return null;
 
@@ -60,7 +72,7 @@ export const OrderInfo: FC = () => {
   }, [orderData, ingredients]);
 
   if (!orderInfo) {
-    return <Preloader />;
+    return;
   }
 
   return <OrderInfoUI orderInfo={orderInfo} />;
--- src/pages/feed/feed.tsx
+++ src/pages/feed/feed.tsx
@@ -1,15 +1,27 @@
 import { Preloader } from '@ui';
 import { FeedUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from '../../services/store';
+import { isLoadingSelector, ordersSelector } from '../../services/order/slice';
+import { getFeeds } from '../../services/order/action';
 
 export const Feed: FC = () => {
   /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const orders: TOrder[] = useSelector(ordersSelector);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(getFeeds());
+  }, []);
 
-  if (!orders.length) {
-    return <Preloader />;
-  }
+  const handleGetFeeds = useCallback(() => {
+    dispatch(getFeeds());
+  }, []);
 
-  <FeedUI orders={orders} handleGetFeeds={() => {}} />;
+  return (
+    <>
+      <FeedUI orders={orders} handleGetFeeds={handleGetFeeds} />
+    </>
+  );
 };
